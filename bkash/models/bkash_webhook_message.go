57394e0fe2d5a4d213750878f2117553be238278
TariqueNasrullah <nasrullahtarique@gmail.com>
2021-05-12 03:21:51 +0600

    bkash build signature refactored

--- bkash/models/bkash_webhook_message.go
+++ bkash/models/bkash_webhook_message.go
@@ -1,19 +1,18 @@
 package models
 
-import "time"
-
 type WebhookData struct {
-	Type             string    `json:"Type"`
-	MessageId        string    `json:"MessageId"`
-	Token            string    `json:"Token"`
-	TopicArn         string    `json:"TopicArn"`
-	Message          string    `json:"Message"`
-	Timestamp        time.Time `json:"Timestamp"`
-	SignatureVersion string    `json:"SignatureVersion"`
-	Signature        string    `json:"Signature"`
-	SigningCertURL   string    `json:"SigningCertURL"`
-	UnsubscribeURL   string    `json:"UnsubscribeURL"`
-	SubscribeURL     string    `json:"SubscribeURL"`
+	Type             string `json:"Type"`
+	MessageId        string `json:"MessageId"`
+	Token            string `json:"Token"`
+	TopicArn         string `json:"TopicArn"`
+	Message          string `json:"Message"`
+	Timestamp        string `json:"Timestamp"`
+	SignatureVersion string `json:"SignatureVersion"`
+	Signature        string `json:"Signature"`
+	SigningCertURL   string `json:"SigningCertURL"`
+	UnsubscribeURL   string `json:"UnsubscribeURL"`
+	SubscribeURL     string `json:"SubscribeURL"`
+	Subject          string `json:"Subject"`
 }
 
 type Message struct {
