e47d6c8ab486c422c0ff715f1fe1b6b4058996aa
TariqueNasrullah <nasrullahtarique@gmail.com>
2021-11-21 18:51:33 +0600

    bkash live gateway url changed

--- bkash/bkash.go
+++ bkash/bkash.go
@@ -17,7 +17,7 @@ import (
 )
 
 const BKASH_SANDBOX_GATEWAY = "https://tokenized.sandbox.bka.sh/v1.2.0-beta"
-const BKASH_LIVE_GATEWAY = "https://tokenized.pay.bka.sh"
+const BKASH_LIVE_GATEWAY = "https://tokenized.pay.bka.sh/v1.2.0-beta"
 const BKASH_GRANT_TOKEN_URI = "/tokenized/checkout/token/grant"
 const BKASH_REFRESH_TOKEN_URI = "/tokenized/checkout/token/refresh"
 const BKASH_CREATE_AGREEMENT_URI = "/tokenized/checkout/create"
